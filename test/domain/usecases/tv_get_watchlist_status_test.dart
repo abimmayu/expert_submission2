753b9a90cfeff5bc355b274bd7dd542a5320cafb
abimmayu <sakilavino@gmail.com>
2022-07-13 14:15:26 +0700

    Add Domain last testing

--- test/domain/usecases/tv_get_watchlist_status_test.dart
+++ test/domain/usecases/tv_get_watchlist_status_test.dart
@@ -1,24 +1,22 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:muse/domain/usecases/get_tv_watchlist.dart';
+import 'package:muse/domain/usecases/get_tv_watchlist_status.dart';
 
-import '../../dummy_data/dummy_objects.dart';
 import '../../helpers/test_tv_helper.mocks.dart';
 
 void main() {
-  late GetWatchlistTv usecase;
+  late GetWatchListStatusTv usecase;
   late MockTvRepository mockTvRepository;
 
   setUp(() {
     mockTvRepository = MockTvRepository();
-    usecase = GetWatchlistTv(mockTvRepository);
+    usecase = GetWatchListStatusTv(mockTvRepository);
   });
 
-  test('should get list of tv from the repository', () async {
-    when(mockTvRepository.getWatchlistTv())
-        .thenAnswer((_) async => Right(testTvList));
-    final result = await usecase.execute();
-    expect(result, Right(testTvList));
+  test('should get watchlist tv status from repository', () async {
+    when(mockTvRepository.isAddedToWatchlistTv(1))
+        .thenAnswer((_) async => true);
+    final result = await usecase.execute(1);
+    expect(result, true);
   });
 }
